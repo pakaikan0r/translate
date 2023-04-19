d0b31337b7f836e831ba0bd956184d401e43cc86
pakaikan0r <130348310+pakaikan0r@users.noreply.github.com>
2023-04-19 09:06:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchangeIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i");
+
+selectTag.forEach((tag,id) =>{
+    for (const country_code in countries) {
+        let selected;
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected";
+        } else if(id == 1 && country_code == "th-TH"){
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}" 
+${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); //เพิ$
+ }
+});
+exchangeIcon.addEventListener("click", () =>{
+    let temptext = fromText.value;
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    selectTag[0].value = selectTag[1].value;
+    toText.value = temptext;
+    selectTag[1].value = tempLang;
+});
+translateBtn.addEventListener("click", () =>{
+    let text = fromText.value,
+    translateForm = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating..."); //ใหเ้ห็นขอ้ความกาํลงัโหลด
+    let apiURL = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateForm}|${translateTo}`;
+    fetch(apiURL).then(res => res.json()).then(data =>{
+        toText.value = data.responseData.translatedText; 
+        toText.setAttribute("placeholder", "Translation");
+    });
+});
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(target.classList.contains("fa-copy")){
+            if(target.id == "from"){
+            navigator.clipboard.writeText(fromText.value);
+        } else {
+            navigator.clipboard.writeText(toText.value);
+        }
+    } else {
+        let utterance; //ตัวพูด
+        if(target.id == "from"){
+            utterance = new SpeechSynthesisUtterance(fromText.value);
+            utterance.lang = selectTag[0].value;
+        } else {
+            utterance = new SpeechSynthesisUtterance(toText.value);
+            utterance.lang = selectTag[1].value;
+            }
+        speechSynthesis.speak(utterance);
+        }
+    });
+});
